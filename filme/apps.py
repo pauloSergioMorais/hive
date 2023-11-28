6540e933bf9bb3bf7f2cf480107bc08328da986f
pauloSergioMorais <paulo.sergio.morais@outlook.com.br>
2023-11-28 00:54:15 -0300

    superuser1

--- filme/apps.py
+++ filme/apps.py
@@ -1,17 +1,19 @@
 from django.apps import AppConfig
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import get_user_model
+import os
 
 
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-    # def ready(self):
-    #     from .models import Usuario
-    #     import os
+    def ready(self):
+        User = get_user_model()
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
 
-    #     email = os.getenv("EMAIL_ADMIN")
-    #     senha = os.getenv("SENHA_ADMIN")
-
-    #     usuarios = Usuario.objects.filter(email=email)
-    #     if not usuarios:
-    #         Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
+        try:
+            usuario = User.objects.get(email=email)
+        except ObjectDoesNotExist:
+            User.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
