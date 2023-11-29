3298518873da460d4ed42da9004adc8e2b40d96c
pauloSergioMorais <paulo.sergio.morais@outlook.com.br>
2023-11-28 23:46:21 -0300

    superuser

--- filme/apps.py
+++ filme/apps.py
@@ -8,4 +8,12 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
     
-    # def ready(self):
+    def ready(self):
+        from .models import Usuario
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
