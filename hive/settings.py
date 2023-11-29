47dec7439d1d70ab9c5436f675db25191f094191
pauloSergioMorais <paulo.sergio.morais@outlook.com.br>
2023-11-28 22:13:38 -0300

    bd

--- hive/settings.py
+++ hive/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_PRIVATE_URL")
 if DATABASE_URL:
   DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
